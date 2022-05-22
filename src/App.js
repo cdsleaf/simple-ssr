eb6d5e86c7c9cc7eaa6fcd57347482df757e929f
Theo Cho <cdsleaf@gmail.com>
2022-05-22 16:35:03 +1200

    update webpack config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import Icon from './icon.png';
 import Home from './Home';
 import About from './About';
 
@@ -33,6 +34,7 @@ export default function App({ page }) {
         About
       </button>
       <PageComponent />
+      <img src={Icon} />
     </Container>
   );
 }
