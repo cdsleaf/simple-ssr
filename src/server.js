09e3d63ae4274d27e2ca2c8ea196d13ed2aeed45
Theo Cho <cdsleaf@gmail.com>
2022-05-24 15:52:24 +1200

    add the logic of the  pre-rendering page

--- src/server.js
+++ src/server.js
@@ -6,25 +6,29 @@ import { renderToString } from 'react-dom/server';
 import React from 'react';
 import { ServerStyleSheet } from 'styled-components';
 import App from './App';
+import { renderPage, prerenderPages } from './common';
 
 const app = express();
-const html = fs.readFileSync(
-  path.resolve(__dirname, '../dist/index.html'),
-  'utf8',
-);
+
+const prerenderHtml = {};
+for (const page of prerenderPages) {
+  const pageHtml = fs.readFileSync(
+    path.resolve(__dirname, '../dist/index.html'),
+    'utf8',
+  );
+  prerenderHtml[page] = pageHtml;
+}
+
 app.use('/dist', express.static('dist'));
 app.get('/favicon.ico', (req, res) => res.sendStatus(204));
 app.get('*', (req, res) => {
   const parsedUrl = url.parse(req.url, true);
   const page = parsedUrl.pathname ? parsedUrl.pathname.substring(1) : 'home';
-  const sheet = new ServerStyleSheet();
-  const renderString = renderToString(sheet.collectStyles(<App page={page} />));
-  const styles = sheet.getStyleTags();
   const initialData = { page };
-  const result = html
-    .replace('<div id="root"></div>', `<div id="root">${renderString}</div>`)
-    .replace('__DATA_FROM_SERVER__', JSON.stringify(initialData))
-    .replace('__STYLE_FROM_SERVER__', styles);
+  const pageHtml = prerenderPages.includes(page)
+    ? prerenderHtml[page]
+    : renderPage(page);
+  const result = pageHtml.replace('__DATA_FROM_SERVER__', JSON.stringify(initialData));
   res.send(result);
 });
 app.listen(3000);
